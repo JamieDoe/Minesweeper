fc33ab83b53e718be8a6368c8af74d103cc9ea4b
Jamie Doe <jamiedoe@AlliantsAdmins-MacBook-Air.local>
2023-10-06 10:11:34 +0100

    dificulty selector

--- Client/main.js
+++ Client/main.js
@@ -1,11 +1,46 @@
 
 // Global Variables
-const rows = 10;
-const  cols = 10;
-const initialArray = [];
+let rows;
+let cols;
+let dificulty;
+
+
+// will be updated to JSX later
+
+const body = document.querySelector("body");
+const userInput = document.createElement("input");
+const submitBtn = document.createElement("button");
+submitBtn.textContent = "Submit";
+submitBtn.onclick = () => {
+  if (userInput.value === "") {
+    alert("Please enter a dificulty");
+    return;
+  }
+  localStorage.setItem("Dificulty", userInput.value);
+  dificulty = localStorage.getItem("Dificulty");
+  dificultySelection();
+}
+body.appendChild(userInput);
+body.appendChild(submitBtn);
+
+// ====================
+
+function dificultySelection() {
+ if (dificulty === "easy") {
+    rows = 10;
+    cols = 10;
+  } else if (dificulty === "medium") {
+    rows = 15;
+    cols = 15;
+  } else if (dificulty === "hard") {
+    rows = 30;
+    cols = 15;
+  }
+  console.log(rows, cols);
+}
 
 // Creates cells
-function createCell(id) {
+function createCell(id, isMine) {
   return {
     isMine: false,
     isRevealed: false,
@@ -14,6 +49,8 @@ function createCell(id) {
   };
 }
 
+
+// Generates mines
 function addMines() {
   const mineArray = [];
 
@@ -25,13 +62,18 @@ function addMines() {
     }
   }
 
+  mineArray.sort((a, b) => a-b);
+
+  console.log(mineArray[0])
+  console.log(mineArray[mineArray.length-1])
+  console.log(mineArray);
   return mineArray;
 }
 
 // Generates gird
 function generateGrid() {
   const board = [];
-  const mineArray = addMines();
+  const mines = addMines();
 
   const body = document.getElementById("body");
 
@@ -41,7 +83,6 @@ function generateGrid() {
     for (let j = 0; j < cols; j++) { 
       row.push(createCell(i * cols + j + 1));
     }
-
     board.push(row);
   }
 
