309736d5795c3939fcb8e957156b0a401d4757c0
Jamie Doe <jamiedoe@AlliantsAdmins-MacBook-Air.local>
2023-10-09 13:09:52 +0100

    Added mine proximity function for neighbouring cells & neighbour id

--- Client/main.js
+++ Client/main.js
@@ -7,6 +7,8 @@
 let rows = 10;
 let cols = 10;
 let cells = [];
+let numOfMines = 10;
+const mineArray = [];
 const body = document.querySelector("body");
 
 // //will be updated to JSX
@@ -38,28 +40,49 @@ const body = document.querySelector("body");
 //   console.log(rows, cols);
 // }
 
-// Creates cells
-
-
-function generateCells(id, isMine) {
+// Creates cell objects with unique properties
+function generateCells(id, isMine, neighbours) {
   return ({
     isMine: isMine || false,
     isRevealed: false,
-    isFlagged: false,
+    //isFlagged: false,
     cellId: id,
+    proximity: 0,
+    neighbourIds: neighbours,
   });
+}
+
+// Gets the neighbours of a cell
+function getNeighbourIds(row, col, rows, cols) {
+  const neighbours = [
+    [-1,-1],[-1,0],[-1,1],
+    [0, -1],/*0,0*/[0, 1],
+    [1, -1],[1, 0],[1, 1]
+  ];
+
+  const neighbourIds = []; // ids of cells at neighbours coords
+
+  neighbours.forEach(([rowOffset, colOffset]) => {
+    const newRow = row + rowOffset;
+    const newCol = col + colOffset;
+    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+      neighbourIds.push(newRow * cols + newCol + 1);
+    }
+  });
+
+  return neighbourIds;
 }
 
 // Generates mines
 function addMines() {
-  const mineArray = [];
-  while (mineArray.length < 10) {
+
+  while (mineArray.length < numOfMines) {
     const randomNum = Math.floor(Math.random() * 100) + 1;
     if (!mineArray.includes(randomNum)) {
       mineArray.push(randomNum);
+      mineArray.sort((a, b) => a - b);
     }
   }
-  mineArray.sort((a, b) => a-b);
   console.log(mineArray);
   return mineArray;
 }
@@ -73,29 +96,51 @@ function generateGrid() {
     const row = [];
     for (let j = 0; j < cols; j++) { 
       const isMine = mines.includes(i * cols + j + 1);
-      const cell = generateCells(i * cols + j + 1, isMine);
+      const neighbours = getNeighbourIds(i, j, rows, cols);
+      const cell = generateCells(i * cols + j + 1, isMine, neighbours);
       cells.push(cell);
       row.push(cell);
     }
     board.push(row);
   }
+  proximity();
   return board
 }
+
+
 
 const gameBoard = generateGrid();
 console.log(gameBoard);
 console.log(cells);
 
+function proximity() {
+  for (let i = 0; i < cells.length; i++) {
+    for (let j = 0; j < cells[i].neighbourIds.length; j++) {
+      const neighbourId = cells[i].neighbourIds[j];
+      const neighbourCell = cells.find(cell => cell.cellId === neighbourId);
+      if (neighbourCell.isMine) {
+        cells[i].proximity++;
+      }
+    }
+  }
+}
+
+
 const cellGrid = document.createElement("div");
 cellGrid.className = "cell-grid";
+
+// Rendering the gameboard/Cells and games functionality
+// Will be updated to JSX when ready
 
 for (let i = 0; i < cells.length; i++) {
   const newCell = document.createElement("div");
   newCell.className = "cell";
   newCell.id = cells[i].cellId;
-  newCell.textContent = cells[i].cellId;
+  newCell.textContent = cells[i].proximity;
   cellGrid.appendChild(newCell);
-
+  if (cells[i].isMine) {
+    newCell.textContent = "💣";
+  }
   newCell.onclick = () => {
   if (cells[i].isMine) {
     cells[i].isRevealed = true;
@@ -109,8 +154,8 @@ for (let i = 0; i < cells.length; i++) {
     cells[i].isRevealed = true;
     newCell.style.backgroundColor = "grey";
     newCell.textContent = "";
-    newCell.style.border = "none"
-  }
+    newCell.style.border = "none";
+}
 }
   body.appendChild(cellGrid);
 }
